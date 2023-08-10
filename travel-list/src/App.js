aa52d873f0cfd9acd556b4f9cfdbed286f6fc5c4
Pratik <pendurkarpratik04@gmail.com>
2023-08-10 20:51:57 +0530

    Setting up the project

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -4,7 +4,7 @@ import Logo from './Logo';
 import Form from './Form';
 import { PackingList } from './PackingList';
 import { Stats } from './Stats';
-
+ 
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
