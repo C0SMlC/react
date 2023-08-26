df824b953defa4e248285e33f4fa2dd80ec39566
Pratik <pendurkarpratik04@gmail.com>
2023-08-26 15:53:05 +0530

    react useRender

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,33 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
 
+const initialState = {
+  questions: [],
+  
+  status: 'loading',
+};
+
+function reducer(state, action) {}
 function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:5000/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question 1</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
