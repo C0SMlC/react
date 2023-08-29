d83a180a49d646be1f94cd7e2bf9b8a51904e035
Pratik <pendurkarpratik04@gmail.com>
2023-08-29 18:59:50 +0530

    minor bug

--- watchit/src/App.js
+++ watchit/src/App.js
@@ -6,10 +6,10 @@ import './index.css';
 import { useLocalStorage } from './useLocalStorage';
 
 const tempMovieData = [
-  { 
+  {
     imdbID: 'tt1375666',
     Title: 'Inception',
-    Year: '2010
+    Year: '2010',
     Poster:
       'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
   },
