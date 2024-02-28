fd5037e499495500e0e799746c17d637498dedf0
C0SMlC <pendurkarpratik04@gmail.com>
2024-02-28 12:49:10 +0530

    Connecting react with redux

--- redux-intro/src/App.js
+++ redux-intro/src/App.js
@@ -1,11 +1,8 @@
+
 import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
-import store from "./store";
-
-store.dispatch({ type: "account/deposit", payload: 500 });
-console.log(store.getState());
 
 function App() {
   return (
--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import App from "./App";
+import store from "./store";
+
+store.dispatch({
+  type: "customer/create",
+  payload: {
+    fullName: "John Doe",
+    nationalId: "1234567890",
+    createdAt: new Date().toISOString(),
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
